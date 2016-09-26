5d4037b8ecd8b9321b353fae74baaa3309c65ee2
luisq2007 <luisq2007@gmail.com>
2016-09-26 15:06:35 -0500

    review assigment 2

--- app.js
+++ app.js
@@ -1,76 +1,99 @@
 (function () {
 'use strict';
 
-var shoppingList_compra = [
-  {
-    name: "Milk",
-    quantity: "2"
-  },
-  {
-    name: "Donuts",
-    quantity: "200"
-  },
-  {
-    name: "Cookies",
-    quantity: "300"
-  },
-  {
-    name: "Chocolate",
-    quantity: "5"
-  },
-  {
-    name: "Pretzel",
-    quantity: "7"
+angular.module('ControllerAsApp', [])
+.controller('ShoppingListController1', ShoppingListController1)
+.controller('ShoppingListController2', ShoppingListController2)
+.factory('ShoppingListFactory', ShoppingListFactory);
+
+// LIST #1 - controller
+ShoppingListController1.$inject = ['ShoppingListFactory'];
+function ShoppingListController1(ShoppingListFactory) {
+  var list1 = this;
+
+  // Use factory to create new shopping list service
+  var shoppingList = ShoppingListFactory();
+
+  list1.items = shoppingList.getItems();
+
+  list1.itemName = "";
+  list1.itemQuantity = "";
+
+  list1.addItem = function () {
+    shoppingList.addItem(list1.itemName, list1.itemQuantity);
   }
-];
 
-var shoppingList_listo = [];
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
-
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
-  $scope.shoppingList_compra = shoppingList_compra;
-  $scope.shoppingList_listo = shoppingList_listo;
-
-  $scope.addToList = function () {
-    var newItem = {
-      name: $scope.newItemName,
-      quantity: $scope.newItemQuantity
-    };
-
-    
+  list1.removeItem = function (itemIndex) {
+    shoppingList.removeItem(itemIndex);
   };
+}
 
-  $scope.Message_buy = function () {
-    if(shoppingList_compra == 0) {
-      return "Everything is bought!"
-    }else {
-      return "";
+
+// LIST #2 - controller
+ShoppingListController2.$inject = ['ShoppingListFactory'];
+function ShoppingListController2(ShoppingListFactory) {
+  var list2 = this;
+
+  // Use factory to create new shopping list service
+  var shoppingList = ShoppingListFactory(3);
+
+  list2.items = shoppingList.getItems();
+
+  list2.itemName = "";
+  list2.itemQuantity = "";
+
+  list2.addItem = function () {
+    try {
+      shoppingList.addItem(list2.itemName, list2.itemQuantity);
+    } catch (error) {
+      list2.errorMessage = error.message;
     }
 
-    
+  }
+
+  list2.removeItem = function (itemIndex) {
+    shoppingList.removeItem(itemIndex);
   };
-  $scope.Message_bought = function () {
-    if (shoppingList_listo.length == 0){
-      return "Nothing bought yet";
-    }else {
-      return "";
+}
+
+
+// If not specified, maxItems assumed unlimited
+function ShoppingListService(maxItems) {
+  var service = this;
+
+  // List of shopping items
+  var items = [];
+
+  service.addItem = function (itemName, quantity) {
+    if ((maxItems === undefined) ||
+        (maxItems !== undefined) && (items.length < maxItems)) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      items.push(item);
     }
-
-    
+    else {
+      throw new Error("Max items (" + maxItems + ") reached.");
+    }
   };
 
-  $scope.removeItem = function (itemIndex) {
-    
-    var newItem = {
-      name: shoppingList_compra[itemIndex].name,
-      quantity: shoppingList_compra[itemIndex].quantity
-    };
-    $scope.shoppingList_listo.push(newItem);
-    $scope.shoppingList_compra.splice(itemIndex, 1);
-
+  service.removeItem = function (itemIndex) {
+    items.splice(itemIndex, 1);
   };
+
+  service.getItems = function () {
+    return items;
+  };
+}
+
+
+function ShoppingListFactory() {
+  var factory = function (maxItems) {
+    return new ShoppingListService(maxItems);
+  };
+
+  return factory;
 }
 
 })();
